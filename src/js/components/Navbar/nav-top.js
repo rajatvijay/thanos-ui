4a042ce12fcbe9c8777e91d3c64becbe2373c3df
junaid7lone <junaid7lone@gmail.com>
2018-07-24 17:59:58 +0530

    fixed

--- src/js/components/Navbar/nav-top.js
+++ src/js/components/Navbar/nav-top.js
@@ -108,8 +108,8 @@ class NavTop extends Component {
                         <span>
                           <Avatar>{user.first_name.charAt(0)}</Avatar>{" "}
                           {user.first_name
-                            ? user.first_name + " " + user.last_name + " "
-                            : user.email}
+                            ? " " + user.first_name + " " + user.last_name + " "
+                            : " " + user.email + " "}
                           <i className="material-icons t-14">
                             keyboard_arrow_down
                           </i>
