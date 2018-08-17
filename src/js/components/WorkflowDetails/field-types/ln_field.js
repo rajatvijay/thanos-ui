87f5d158d2d4e3115b4b7ed86b15bb04a4a76337
junaid7lone <junaid7lone@gmail.com>
2018-08-17 10:05:27 +0530

    lexis nexis details

--- src/js/components/WorkflowDetails/field-types/ln_field.js
+++ src/js/components/WorkflowDetails/field-types/ln_field.js
@@ -124,8 +124,312 @@ class LexisNexisSearch extends Component {
   };
 }
 
+const DescriptionItem = ({ title, content }) => (
+  <div
+    style={{
+      fontSize: 14,
+      lineHeight: "22px",
+      marginBottom: 7,
+      color: "rgba(0,0,0,0.65)"
+    }}
+  >
+    <p
+      style={{
+        marginRight: 8,
+        display: "inline-block",
+        color: "rgba(0,0,0,0.85)"
+      }}
+    >
+      {title}:
+    </p>
+    {content}
+  </div>
+);
+
+const buildDetails = obj => {
+  obj["id"] = obj["ID"]["$"];
+
+  var akas = obj["EntityDetails"]["AKAs"]
+    ? obj["EntityDetails"]["AKAs"]["EntityAKA"]
+    : [];
+  akas = _.map(akas, function(aka) {
+    if (!aka["Name"] || !aka["Name"]["Full"]) {
+      return <span>-</span>;
+    }
+    return (
+      <span>
+        <span>
+          &nbsp;<b>AKA: </b>
+          {aka["Name"]["Full"]["$"]}
+        </span>
+        <br />
+      </span>
+    );
+  });
+
+  var comments = "-";
+  if (obj["EntityDetails"]["Comments"]) {
+    var comments_list = obj["EntityDetails"]["Comments"]["$"].split("||");
+    comments = _.map(comments_list, function(c) {
+      return (
+        <span>
+          <span>{c.trim()}</span>
+          <br />
+        </span>
+      );
+    });
+  }
+
+  var additional_info = "-";
+  if (
+    obj["EntityDetails"]["AdditionalInfo"] &&
+    obj["EntityDetails"]["AdditionalInfo"]["EntityAdditionalInfo"]
+  ) {
+    additional_info = _.map(
+      obj["EntityDetails"]["AdditionalInfo"]["EntityAdditionalInfo"],
+      function(eai) {
+        var type = eai["Type"] ? eai["Type"]["$"] + " Information" : "-";
+        var value = eai["Value"] ? eai["Value"]["$"] : "-";
+        var comments = eai["Comments"] ? eai["Comments"]["$"].split("|") : [];
+        return (
+          <span>
+            <span>
+              &nbsp;<b>{type}</b>: {value}
+            </span>
+            <br />
+            <span>
+              &nbsp;Comments:{" "}
+              {_.map(comments, function(c) {
+                return (
+                  <span>
+                    <span>{c.trim()}</span>
+                    <br />
+                  </span>
+                );
+              })}
+            </span>
+            <br />
+          </span>
+        );
+      }
+    );
+  }
+
+  var source_date = "-";
+  if (
+    obj["File"] &&
+    obj["File"]["Published"] &&
+    obj["File"]["Published"]["$"]
+  ) {
+    source_date = new Date(obj["File"]["Published"]["$"]).toLocaleDateString();
+  }
+
+  var number = "-";
+  if (
+    obj["EntityDetails"]["IDs"] &&
+    obj["EntityDetails"]["IDs"]["EntityID"] &&
+    obj["EntityDetails"]["IDs"]["EntityID"]["Number"] &&
+    obj["EntityDetails"]["IDs"]["EntityID"]["Number"]["$"]
+  ) {
+    number = obj["EntityDetails"]["IDs"]["EntityID"]["Number"]["$"];
+  }
+
+  var addresses = "-";
+  if (
+    obj["EntityDetails"]["Addresses"] &&
+    obj["EntityDetails"]["Addresses"]["EntityAddress"]
+  ) {
+    addresses = _.map(
+      obj["EntityDetails"]["Addresses"]["EntityAddress"],
+      function(adr) {
+        var addr = [
+          adr["Street1"] ? adr["Street1"]["$"] : "",
+          adr["City"] ? adr["City"]["$"] : "",
+          adr["StateProvinceDistrict"] ? adr["StateProvinceDistrict"]["$"] : "",
+          adr["PostalCode"] ? adr["PostalCode"]["$"] : ""
+        ];
+        return (
+          <span>
+            &nbsp;<b>Address: </b> <span>{addr.join(" ")}</span>
+            <br />
+            &nbsp;&nbsp;<span>Country: </span>{" "}
+            <span>{adr["Country"] ? adr["Country"]["$"] : ""}</span>
+            <br />
+          </span>
+        );
+      }
+    );
+  }
+
+  var ids = "-";
+  if (obj["EntityDetails"]["IDs"] && obj["EntityDetails"]["IDs"]["EntityID"]) {
+    var ids_arr = obj["EntityDetails"]["IDs"]["EntityID"];
+    if (Object.prototype.toString.call(ids_arr) == "[object Object]") {
+      ids_arr = [ids_arr];
+    }
+    ids = _.map(ids_arr, function(id) {
+      return (
+        <span>
+          &nbsp;<b>{id["Type"]["$"]}: </b>
+          <span>{id["Number"]["$"]}</span>
+          <br />
+        </span>
+      );
+    });
+  }
+
+  obj["additional_info_text"] = additional_info;
+  obj["comments_text"] = comments;
+  obj["akas_text"] = akas;
+  obj["addresses_text"] = addresses;
+
+  return (
+    <div className="dnb-rdc-wrapper">
+      <div className="match-item company-item">
+        <h4 className="match-title mr-bottom-lg">{obj["EntityName"]["$"]}</h4>
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="Added To Accept List"
+              content={!obj["AddedToAcceptList"]["$"] ? "False" : "True"}
+            />
+          </Col>
+          <Col span={12}>
+            <DescriptionItem
+              title="Address Name"
+              content={!obj["AddressName"]["$"] ? "False" : "True"}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="Auto False Positive"
+              content={!obj["AutoFalsePositive"]["$"] ? "False" : "True"}
+            />
+          </Col>
+          <Col span={12}>
+            <DescriptionItem
+              title="Best Address Is Partial"
+              content={!obj["BestAddressIsPartial"]["$"] ? "False" : "True"}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="Best Country Scoree"
+              content={obj["BestCountryScore"]["$"]}
+            />
+          </Col>
+          <Col span={12}>
+            <DescriptionItem
+              title="Best Country Type"
+              content={obj["BestCountryType"]["$"]}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="Best DOB Is Partial"
+              content={obj["BestCountryScore"]["$"]}
+            />
+          </Col>
+          <Col span={12}>
+            <DescriptionItem title="Best Name" content={obj["BestName"]["$"]} />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="Best Name Score"
+              content={obj["BestNameScore"]["$"]}
+            />
+          </Col>
+          <Col span={12}>
+            <DescriptionItem
+              title="False Positive"
+              content={!obj["FalsePositive"]["$"] ? "False" : "True"}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="Match ReAlert"
+              content={!obj["MatchReAlert"]["$"] ? "False" : "True"}
+            />
+          </Col>
+          <Col span={12}>
+            <DescriptionItem
+              title="Gateway OFAC Screening Indicator Match"
+              content={
+                !obj["GatewayOFACScreeningIndicatorMatch"]["$"]
+                  ? "False"
+                  : "True"
+              }
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <DescriptionItem
+              title="True Match"
+              content={!obj["TrueMatch"]["$"] ? "False" : "True"}
+            />
+          </Col>
+          <Col span={12} />
+        </Row>
+
+        <Divider />
+        <div className="col-sm-12">
+          <h4 className="match-label ">AKAs:</h4>
+          <br />
+          <span className="match-value">{obj["akas_text"]}</span>
+          <br />
+        </div>
+
+        <Divider />
+
+        <div className="col-sm-12">
+          <h4 className="match-label">Comments:</h4>
+          <br />
+          <span className="match-value">{obj["comments_text"]}</span>
+          <br />
+        </div>
+
+        <Divider />
+        <div className="col-sm-12">
+          <h4 className="match-label">Addresses:</h4>
+          <br />
+          <span className="match-value">{obj["addresses_text"]}</span>
+          <br />
+        </div>
+
+        <Divider />
+        <div className="col-sm-12">
+          <h4 className="match-label">Additional Info:</h4>
+          <br />
+          <span className="match-value">{obj["additional_info_text"]}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const GetTable = props => {
   const data = props.jsonData;
+
+  console.log("props----------------------");
+  console.log(props);
+
   const columns = [
     {
       title: "Name",
@@ -183,10 +487,14 @@ const GetTable = props => {
         dataSource={data}
         pagination={false}
         columns={columns}
+        rowKey="uid"
         // whole structure is required for the extra data available
-        /*expandedRowRender={record => (
-          <p style={{ margin: 0 }}>This is more data</p>
-        )}*/
+        expandedRowRender={record => buildDetails(record)}
+        // onRow={(record) => ({
+        //   onClick: () => {
+        //     this.selectRow(record);
+        //   },
+        // })}
       />
     </div>
   );
