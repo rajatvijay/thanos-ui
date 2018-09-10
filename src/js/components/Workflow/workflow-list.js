528ff59f3e70029c1c2be9d77d2039a7e24181b3
junaid7lone <junaid7lone@gmail.com>
2018-09-10 17:47:08 +0530

    change status and quick overview label

--- src/js/components/Workflow/workflow-list.js
+++ src/js/components/Workflow/workflow-list.js
@@ -248,10 +248,29 @@ class WorkflowItem extends React.Component {
         {/*show children here */}
         {hasChildren && this.state.showRelatedWorkflow ? (
           <div className="child-workflow-wrapper mr-top">
-            <ChildWorkflow
-              {...this.props}
-              getGroupedData={this.getGroupedData}
-            />
+            {_.map(this.getGroupedData(this.props.workflow.children), function(
+              childGroup
+            ) {
+              return (
+                <div className="mr-bottom">
+                  {_.map(childGroup, function(item, index) {
+                    let proccessedData = getProcessedData(item.step_groups);
+                    return (
+                      <WorkflowItem
+                        isChild={true}
+                        pData={proccessedData}
+                        workflow={item}
+                        key={index}
+                        kinds={that.props.kinds}
+                        dispatch={that.props.dispatch}
+                        workflowFilterType={that.props.workflowFilterType}
+                        statusView={that.props.statusView}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            })}
           </div>
         ) : null}
 
