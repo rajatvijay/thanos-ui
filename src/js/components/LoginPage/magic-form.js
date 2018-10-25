ec7e49a43f4e1da15c2817f241674e4b05405306
junaid7lone <junaid7lone@gmail.com>
2018-10-25 06:47:45 +0530

    magic link txt change

--- src/js/components/LoginPage/magic-form.js
+++ src/js/components/LoginPage/magic-form.js
@@ -89,7 +89,7 @@ class LoginLinkForm extends React.Component {
         {!this.props.emailAuth.submitted ? (
           <div>
             <div className="text-center mr-bottom t-18 text-base">
-              Enter your email address to login
+              Enter your email to recieve one-time sign in link
             </div>
             <Form
               layout="vertical"
