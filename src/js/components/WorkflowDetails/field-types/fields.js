7483843db89ec10858053106578ff2e556ce1df3
junaid7lone <junaid7lone@gmail.com>
2018-10-31 07:10:41 +0530

    search by label

--- src/js/components/WorkflowDetails/field-types/fields.js
+++ src/js/components/WorkflowDetails/field-types/fields.js
@@ -431,6 +431,10 @@ export const Select = props => {
         }
         onChange={save}
         showSearch={true}
+        filterOption={(input: any, option: any) =>
+          option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+        //filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
       >
         {_.map(props.field.definition.extra, function(item, index) {
           return (
