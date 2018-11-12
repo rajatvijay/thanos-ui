be5603cfaecfbdf0cd8a9461f4749ef538a4554d
junaid7lone <junaid7lone@gmail.com>
2018-11-12 14:37:21 +0530

    revert develpment changes

--- src/js/components/WorkflowDetails/field-types/index.js
+++ src/js/components/WorkflowDetails/field-types/index.js
@@ -32,7 +32,6 @@ import { GoogleSearch } from "./google-search";
 import { SerpSearch } from "./serpapi-search";
 import { UBOGraph } from "./dnb_ubo_graph";
 import { TRResults } from "./tr_results";
-import { ChildWorkflowField } from "./ChildWorkflow";
 
 export const getFieldType = props => {
   switch (props.field.definition.field_type) {
@@ -142,8 +141,6 @@ export const getFieldType = props => {
       return TRResults(props);
     case "thomson_reuters_wcprofile":
       return DnBCommon(props);
-    case "child_workflow":
-      return ChildWorkflowField(props);
 
     default:
       return Text(props);
