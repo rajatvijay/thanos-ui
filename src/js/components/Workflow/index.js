d438d39c38308a3205440684e20df6036e804460
junaid <junaid7lone@gmail.com>
2019-01-08 11:17:16 +0530

    something broke now reverting.

--- src/js/components/Workflow/index.js
+++ src/js/components/Workflow/index.js
@@ -38,7 +38,6 @@ class Workflow extends Component {
       isUserAuthenticated: false,
       statusView: true,
       visible: false,
-      showRisk: false,
       sortOrderAsc: false
     };
 
@@ -214,11 +213,12 @@ class Workflow extends Component {
       showInsights = true;
     }
 
+    let showRisk = false;
     if (
       _.size(this.props.workflow.workflow) &&
       this.props.workflow.workflow[0].sorting_primary_field
     ) {
-      this.setState({ showRisk: true });
+      showRisk = true;
     }
 
     return (
@@ -355,7 +355,7 @@ class Workflow extends Component {
                 <Row>
                   <Col span={19} />
                   <Col span={5}>
-                    {this.state.showRisk ? (
+                    {showRisk ? (
                       <Tooltip
                         title={
                           this.state.sortOrderAsc
