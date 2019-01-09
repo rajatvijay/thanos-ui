db274ab90ef4585b4ac680234d1bdec592fe65d1
harshattray <harsh.attray@gmail.com>
2019-01-09 14:08:34 +0530

    i18n configured | Data source added

---
+++ src/js/components/common/intlMessages.js
@@ -0,0 +1,33 @@
+export default {
+  "en-US": {
+    workflowsInstances: {
+      waitingDropdown: "Waiting on",
+      detailsViewToggle: "Details view",
+      workflowViewToggle: "Workflow view",
+      workflowsCount: "Workflows",
+      noWorkflowsError: "Unable to load workflow list",
+      loggedOutError:
+        "You have been logged out. Please Login again to continue",
+      clickToLogin: "Click here to Login"
+    }
+  },
+  es: {
+    workflowsInstances: {
+      waitingDropdown: "Esperando que",
+      detailsViewToggle: "Vista detallada:",
+      workflowViewToggle: "Vista de flujo de trabajo",
+      workflowsCount: "Flujos de trabajo",
+      noWorkflowsError: "No se puede cargar la lista de flujo de trabajo",
+      loggedOutError: "Haga clic aquí para ingresar",
+      clickToLogin: "Click here to Login"
+    }
+  },
+  fr: {
+    workflowsInstances: {
+      waitingDropdown: "Basculer",
+      detailsViewToggle: "Achetez ce livre à partir de:",
+      workflowViewToggle: "Avis",
+      workflowsCount: "Flujos de trabajo"
+    }
+  }
+};
