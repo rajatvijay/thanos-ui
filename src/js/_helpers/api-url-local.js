8ea6b22791d79b96090c48fe806d534eafd9988a
harshattray <harsh.attray@gmail.com>
2019-01-12 14:55:43 +0530

    cleanup

--- src/js/_helpers/api-url-local.js
+++ src/js/_helpers/api-url-local.js
@@ -4,12 +4,10 @@ const getSite = site => {
   let domain =
     document.location.protocol +
     "//" +
-    hostSplit[0] +
-    "." +
     hostSplit[1] +
     "." +
     hostSplit[2] +
-    ":8000/api/v1/";
+    "/api/v1/";
   return domain;
 };
 
