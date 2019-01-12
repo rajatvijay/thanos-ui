fd6e9c05c1ed0582e81bbc809acc767fcb9ace84
Ambarish Chaudhari <ambarish.chaudhari@gmail.com>
2019-01-13 03:07:45 +0530

    Added languages list, temp Spanish translations and fallback to default locale

--- src/js/App.js
+++ src/js/App.js
@@ -6,44 +6,49 @@ import { loadReCaptcha } from "react-recaptcha-v3";
 import { connect } from "react-redux";
 import messages from "./components/common/intlMessages";
 import { addLocaleData, IntlProvider, injectIntl } from "react-intl";
-import en from "react-intl/locale-data/en";
-import es from "react-intl/locale-data/es";
-import fr from "react-intl/locale-data/fr";
 import { flattenMessages } from "./components/common/messageUtils";
-import { languageActions } from "./actions";
-
-addLocaleData([...en, ...es, ...fr]);
+import { languageConstants } from "./constants";
 
 class App extends React.Component {
   componentDidMount() {
     loadReCaptcha("6LeIoHkUAAAAANZKP5vkvU-B2uEuJBhv13_6h9-8");
-    let preferredLanguage = this.props.config.prefered_language || "en";
-    this.props.dispatch(languageActions.updateUserLanguage(preferredLanguage));
   }
 
   render = () => {
+    let user = this.props.authentication.user;
     let locale =
-      this.props.config.prefered_language ||
+      (user && user.prefered_language) ||
       this.props.languageSelector.language ||
       (navigator.languages && navigator.languages[0]) ||
       navigator.language ||
       navigator.userLanguage ||
-      "en-US";
-    let messageTranslate = messages[locale] || messages["en-US"];
+      languageConstants.DEFAULT_LOCALE;
+    addLocaleData(require(`react-intl/locale-data/${locale}`));
+    let messagesDefaultLocale = flattenMessages(
+      messages[languageConstants.DEFAULT_LOCALE]
+    );
+    let messageTranslate = messagesDefaultLocale;
+    console.log(locale);
+    if (messages[locale]) {
+      messageTranslate = Object.assign(
+        {},
+        messagesDefaultLocale,
+        flattenMessages(messages[locale])
+      );
+    }
+    console.log(messageTranslate);
     return (
-      <IntlProvider
-        locale={locale}
-        messages={flattenMessages(messageTranslate)}
-      >
+      <IntlProvider locale={locale} messages={messageTranslate}>
         <Routes />
       </IntlProvider>
     );
   };
 }
 function mapStateToProps(state) {
-  const { languageSelector, config } = state;
+  const { languageSelector, authentication, config } = state;
   return {
     languageSelector,
+    authentication,
     config
   };
 }
