5a20be33caaa8077bb3ff98008a6b74c899beceb
junaid <junaid7lone@gmail.com>
2019-01-27 11:54:30 +0530

    added flags in dropdown for language selection

--- src/js/components/SelectLanguage/index.js
+++ src/js/components/SelectLanguage/index.js
@@ -36,7 +36,6 @@ class SelectLanguage extends React.Component {
         <Select
           defaultValue={preferredLanguage}
           style={{
-            width: 110,
             paddingTop: "20px",
             float: this.props.navbar ? "right" : "",
             lineHeight: this.props.navbar ? "62px" : ""
