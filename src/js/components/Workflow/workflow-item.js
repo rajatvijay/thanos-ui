01c4ee6a8689bd34b5c11577470dba6a0106fcf7
junaid <junaid7lone@gmail.com>
2019-02-18 12:41:12 +0530

    return subtext value not the object

--- src/js/components/Workflow/workflow-item.js
+++ src/js/components/Workflow/workflow-item.js
@@ -86,7 +86,9 @@ const HeaderTitle = props => {
           </Link>
         )}
 
-        <div className="lc1 text-ellipsis ">{subtext ? subtext[0] : ""}</div>
+        <div className="lc1 text-ellipsis ">
+          {_.size(subtext) ? subtext[0].value : ""}
+        </div>
       </div>
     </Col>
   );
