4b21f7cf41d4706bd9ab7fc1b31093a3ad31ea8c
Ambarish Chaudhari <ambarish.chaudhari@gmail.com>
2019-03-06 14:49:06 +0530

    Changed order of filters (region <-> bu)

--- src/js/components/Workflow/filter.js
+++ src/js/components/Workflow/filter.js
@@ -33,41 +33,15 @@ const FormItem = Form.Item;
 const filterTypeSelect = [
   {
     filterType: "status",
-    filterName: "Status",
-    results: [
-      { id: 1, filterType: "status", value: "main", label: "Main Group" },
-      { id: 2, filterType: "status", value: "ae_uae_ou", label: "AE UAE OU" },
-      { id: 3, filterType: "status", value: "AO_AGO_OU", label: "AO AGO OU" },
-      { id: 4, filterType: "status", value: "AO_ANG_OU", label: "AO ANG OU" }
-    ]
-  },
-  {
-    filterType: "Business",
-    filterName: "business unit",
-    results: [
-      { id: 1, filterType: "bu", value: "main", label: "Main Group" },
-      { id: 2, filterType: "bu", value: "ae_uae_ou", label: "AE UAE OU" },
-      { id: 3, filterType: "bu", value: "AO_AGO_OU", label: "AO AGO OU" },
-      { id: 4, filterType: "bu", value: "AO_ANG_OU", label: "AO ANG OU" },
-      {
-        id: 5,
-        filterType: "bu",
-        value: "AR_ARS_USD_OU",
-        label: "AR ARS USD OU"
-      },
-      { id: 6, filterType: "bu", value: "AT_LIX_OU", label: "AT LIX OU" },
-      { id: 7, filterType: "bu", value: "AU_RTA_OU", label: "AU RTA OU" }
-    ]
+    filterName: "Status"
   },
   {
     filterType: "region",
-    filterName: "region",
-    results: [
-      { id: 1, filterType: "region", value: "EMEA", label: "EMEA" },
-      { id: 2, filterType: "region", value: "LATAM", label: "LATAM" },
-      { id: 3, filterType: "region", value: "APAC", label: "APAC" },
-      { id: 4, filterType: "region", value: "US", label: "US" }
-    ]
+    filterName: "region"
+  },
+  {
+    filterType: "Business",
+    filterName: "business unit"
   }
 ];
 
@@ -227,7 +201,7 @@ class WorkflowKindFilter extends Component {
     this.state = {
       selected: null,
       initialLoad: true,
-      value:null
+      value: null
     };
   }
 
@@ -245,22 +219,24 @@ class WorkflowKindFilter extends Component {
   };
 
   handleChange = value => {
-    let id = parseInt(value,10);
+    let id = parseInt(value, 10);
     let that = this;
     this.setState({ value });
-    let metaValue= _.find(this.props.workflowKind.workflowKind,item => {return item.id === id});
-    
-    let payload = { filterType: "kind", filterValue: [id],meta: metaValue  };
+    let metaValue = _.find(this.props.workflowKind.workflowKind, item => {
+      return item.id === id;
+    });
+
+    let payload = { filterType: "kind", filterValue: [id], meta: metaValue };
     this.props.dispatch(workflowFiltersActions.setFilters(payload));
-    setTimeout(function(){
+    setTimeout(function() {
       that.fetchGroupData(metaValue.tag);
-    },300)
+    }, 300);
   };
 
   fetchGroupData = tag => {
     this.props.dispatch(workflowKindActions.getCount(tag));
     this.props.dispatch(workflowKindActions.getAlertCount(tag));
-    this.props.dispatch(workflowKindActions.getStatusCount(tag))
+    this.props.dispatch(workflowKindActions.getStatusCount(tag));
   };
 
   render = () => {
@@ -268,30 +244,31 @@ class WorkflowKindFilter extends Component {
     let workflowKind = this.props.workflowKind.workflowKind;
     const { value } = this.state;
 
-    const options = workflowKind ? _.map(workflowKind,d => <Option key={d.id}  >{d.name}</Option>):[];
+    const options = workflowKind
+      ? _.map(workflowKind, d => <Option key={d.id}>{d.name}</Option>)
+      : [];
 
-    return (<div className="aux-item aux-lead filter-title">
-       
-
-         <FormItem
-          label={<FormattedMessage id="workflowFiltersTranslated.filterWorkflowType" />}>
-
-            <Select
-              mode="single"
-         
-              value={value}
-              placeholder="Select Workflow Type"
-              onChange={this.handleChange}
-              onDeselect={this.onDeselect}
-              onSelect={this.onSelect}
-              style={{ width: "100%" }}
-              //allowClear={true}
-              //labelInValue={true}
-            
-            >
-              {options}
-            </Select>
-          </FormItem>
+    return (
+      <div className="aux-item aux-lead filter-title">
+        <FormItem
+          label={
+            <FormattedMessage id="workflowFiltersTranslated.filterWorkflowType" />
+          }
+        >
+          <Select
+            mode="single"
+            value={value}
+            placeholder="Select Workflow Type"
+            onChange={this.handleChange}
+            onDeselect={this.onDeselect}
+            onSelect={this.onSelect}
+            style={{ width: "100%" }}
+            //allowClear={true}
+            //labelInValue={true}
+          >
+            {options}
+          </Select>
+        </FormItem>
       </div>
     );
   };
@@ -444,13 +421,13 @@ class FilterSidebar extends Component {
           <br />
 
           <div className=" section-kind">
-
-            {this.props.workflowKind?
+            {this.props.workflowKind ? (
               <WorkflowKindFilter
                 workflowKind={this.props.workflowKind}
                 workflowFilters={this.props.workflowFilters}
                 dispatch={this.props.dispatch}
-              />:null}
+              />
+            ) : null}
           </div>
 
           <div className="filter-section">
