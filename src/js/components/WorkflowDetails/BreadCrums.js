2b2939543eccffc5d2844c056337fbd28ecc76ec
Rajat Vijay <rajatvijay5@gmail.com>
2019-03-14 14:24:33 +0530

    UI changes to the breadcrums

--- src/js/components/WorkflowDetails/BreadCrums.js
+++ src/js/components/WorkflowDetails/BreadCrums.js
@@ -1,37 +1,56 @@
 import React from "react";
 import { workflowDetailsService } from "../../services";
+import { Icon } from "antd";
 
 class BreadCrums extends React.Component {
   state = {
+    isLoading: false,
     items: null
   };
 
-  componentDidUpdate(previousProps) {
-    if (this.props.workflowId !== previousProps.workflowId) {
-      workflowDetailsService
-        .getById(this.props.workflowId)
-        .then(response => {
-          this.setState({ items: response.workflow_family });
-        })
-        .catch(err => {});
-    }
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    workflowDetailsService
+      .getById(this.props.workflowId)
+      .then(response => {
+        this.setState({ isLoading: false });
+        this.setState({ items: response.workflow_family });
+      })
+      .catch(err => {
+        this.setState({ isLoading: false });
+      });
   }
 
   render() {
-    const { items } = this.state;
+    const { items, isLoading } = this.state;
+    console.log("redering", isLoading);
+
+    if (isLoading) {
+      return (
+        <div className="text-center">
+          <Icon type="loading" />
+        </div>
+      );
+    }
 
     if (!items || !items.length) {
       return null;
     }
 
     return (
-      <div>
-        {items.map((item, index) => (
-          <span>
-            {index !== 0 ? ">" : null}
-            {item}
-          </span>
-        ))}
+      <div
+        style={{
+          fontSize: 12,
+          marginTop: -15,
+          borderTop: "1px solid #eee",
+          borderBottom: "1px solid #eee",
+          marginLeft: -15,
+          marginRight: -15,
+          padding: "2px 80px",
+          color: "#305ebe"
+        }}
+      >
+        {items.map(item => <span>{`${item.name} >`}</span>)}
       </div>
     );
   }
