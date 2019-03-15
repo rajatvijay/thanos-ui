3a7c42c09e8d1ea5a3e3e580cfbbfd4d7c83cd5d
junaid <junaid7lone@gmail.com>
2019-03-15 16:19:56 +0530

    added loader for integration auto submit

--- src/js/components/WorkflowDetails/field-types/dnb_directors.js
+++ src/js/components/WorkflowDetails/field-types/dnb_directors.js
@@ -71,7 +71,10 @@ class DnbDirectors extends Component {
     };
 
     let final_html = null;
-    if (this.props.currentStepFields.integration_data_loading) {
+    if (
+      this.props.currentStepFields.integration_data_loading ||
+      field.integration_json.status_message == "Fetching data for this field..."
+    ) {
       final_html = (
         <div>
           <div className="text-center mr-top-lg">
