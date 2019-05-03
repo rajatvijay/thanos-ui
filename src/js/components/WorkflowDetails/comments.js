abf028f8274eb8f906f1af704b959ddf300e730c
ayushikhetan <ayushikhetan11@gmail.com>
2019-05-03 11:59:30 +0530

    issue fix

--- src/js/components/WorkflowDetails/comments.js
+++ src/js/components/WorkflowDetails/comments.js
@@ -16,7 +16,8 @@ import {
   Dropdown,
   Cascader,
   Upload,
-  Modal
+  Modal,
+  Tooltip
 } from "antd";
 import { Link } from "react-router-dom";
 import { workflowDetailsActions, changeStatusActions } from "../../actions";
@@ -465,16 +466,21 @@ class Comments extends Component {
                                 : msg.posted_by.email}
                             </b>
                             <span
-                              style={{ fontSize: "11px", marginLeft: "6px" }}
+                              style={{
+                                fontSize: "11px",
+                                marginLeft: "6px",
+                                cursor: "pointer"
+                              }}
                             >
-                              <Moment fromNow>{msg.created_at}</Moment>
-                            </span>
-                            <span
-                              style={{ fontSize: "11px", marginLeft: "6px" }}
-                            >
-                              (<Moment format="DD/MM/YYYY, h:mm a">
-                                {msg.created_at}
-                              </Moment>)
+                              <Tooltip
+                                title={
+                                  <Moment format="DD/MM/YYYY, h:mm a">
+                                    {msg.created_at}
+                                  </Moment>
+                                }
+                              >
+                                <Moment fromNow>{msg.created_at}</Moment>
+                              </Tooltip>
                             </span>
                           </div>
                           <p style={{ fontSize: "12px", paddingLeft: "32px" }}>
