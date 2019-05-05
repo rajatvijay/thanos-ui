1103703330e7badb9c1dc09d245e5ee4c47114fd
Ayushi Khetan <ayushikhetan11@gmail.com>
2019-05-05 16:34:26 +0530

    Header design

---
+++ src/modules/header/intlLanguages.js
@@ -0,0 +1,36 @@
+export default {
+  endonyms: {
+    en: "English",
+    es: "Español",
+    "es-cl": "Español Chileno",
+    "es-419": "Español Latino",
+    pt: "Português",
+    ru: "русский",
+    fr: "Français",
+    ja: "日本語 (にほんご)",
+    ko: "한국어",
+    ms: "Bahasa Melayu",
+    th: "ไทย",
+    vi: "Tiếng Việt",
+    id: "Bahasa Indonesia",
+    "zh-cn": "简体中文",
+    "zh-tw": "正體中文"
+  },
+  names: {
+    en: "English",
+    es: "Spanish",
+    "es-cl": "Spanish Chilean",
+    "es-419": "Spanish Latin",
+    pt: "Portuguese",
+    ru: "Russian",
+    fr: "French",
+    ja: "Japanese",
+    ko: "Korean",
+    ms: "Malay",
+    th: "Thai",
+    vi: "Vietnamese",
+    id: "Indonesian",
+    "zh-cn": "Chinese (Simplified)",
+    "zh-tw": "Chinese (Traditional)"
+  }
+};
