d8dfb3b99f1d4b70f6097feb92bdb704e96638bb
Ayushi Khetan <ayushikhetan11@gmail.com>
2019-05-07 11:38:58 +0530

    WIP

--- src/modules/header/components/Header.js
+++ src/modules/header/components/Header.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 
-import walmartLOgo from "../../../images/client-logo/walmart_logo.png";
+import { connect } from "react-redux";
+import { injectIntl } from "react-intl";
+import { authHeader, baseUrl, baseUrl2 } from "../../../js/_helpers";
+import { Tooltip } from "antd";
 
 class Header extends Component {
   render() {
+    console.log(this.props);
+    let user = this.props.authentication.user;
     return (
       <div
         style={{
@@ -19,16 +24,23 @@ class Header extends Component {
           alignItems: "center"
         }}
       >
-        <img
-          src={walmartLOgo}
-          alt="walmart logo"
-          style={{
-            marginLeft: "17px",
-            marginTop: "7px",
-            marginBottom: "5px",
-            height: "42.5px"
-          }}
-        />
+        {!this.props.config.loading && this.props.config.logo ? (
+          <img
+            alt={this.props.config.name}
+            src={this.props.config.logo}
+            style={{
+              marginLeft: "17px",
+              marginTop: "7px",
+              marginBottom: "5px",
+              height: "42.5px"
+            }}
+          />
+        ) : !this.props.config.loading ? (
+          <h3>{this.props.config.name}</h3>
+        ) : (
+          <h3>{authHeader.getClient()}</h3>
+        )}
+
         <div
           style={{
             marginRight: "10px",
@@ -73,16 +85,37 @@ class Header extends Component {
           >
             EN
           </span>
-          <i
-            className="material-icons text-middle"
-            style={{ fontSize: 36, color: "#000000", opacity: 0.3 }}
-          >
-            account_circle
-          </i>
+          {user ? (
+            <Tooltip title={user.email} placement="leftBottom">
+              <i
+                className="material-icons text-middle"
+                style={{ fontSize: 36, color: "#000000", opacity: 0.3 }}
+              >
+                account_circle
+              </i>
+            </Tooltip>
+          ) : null}
         </div>
       </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  const {
+    workflowKind,
+    authentication,
+    config,
+    languageSelector,
+    showFilterMenu
+  } = state;
+  return {
+    workflowKind,
+    authentication,
+    config,
+    languageSelector,
+    showFilterMenu
+  };
+}
+
+export default connect(mapStateToProps)(injectIntl(Header));
