e747628eb1e240152d7ff5a1f11aae2e5e4cb2e6
Ayushi Khetan <ayushikhetan11@gmail.com>
2019-05-07 18:14:09 +0530

    Header design

--- src/modules/header/components/Header.js
+++ src/modules/header/components/Header.js
@@ -3,12 +3,56 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { injectIntl } from "react-intl";
 import { authHeader, baseUrl, baseUrl2 } from "../../../js/_helpers";
-import { Tooltip } from "antd";
+import { Tooltip, Menu, Dropdown, Input, Icon } from "antd";
+import SelectLanguage from "./SelectLanguage";
+import _ from "lodash";
+import { logout, workflowActions } from "../../../js/actions";
 
 class Header extends Component {
+  state = {
+    searchInput: "",
+    showSearchInputIcon: false
+  };
+
+  onSearch = e => {
+    if (e) {
+      this.props.dispatch(workflowActions.searchWorkflow(e));
+    } else {
+      this.props.dispatch(workflowActions.getAll());
+    }
+  };
+
+  onSearchChange = e => {
+    this.setState({ searchInput: e.target.value });
+  };
+
+  emitEmpty = () => {
+    this.searchInput.focus();
+    this.setState({ searchInput: "" });
+    this.props.dispatch(workflowActions.getAll());
+  };
+
+  handleKeyPress = e => {
+    if (e.key === "Enter") {
+      this.onSearch(this.state.searchInput);
+    }
+  };
+
+  toggleSearchIcon = () => {
+    this.setState(({ showSearchInputIcon }) => ({
+      showSearchInputIcon: !showSearchInputIcon
+    }));
+  };
+
+  onLogout = (event, key) => {
+    // this.props.dispatch(logout());
+    logout();
+  };
+
   render() {
-    console.log(this.props);
+    const { searchInput, showSearchInputIcon } = this.state;
     let user = this.props.authentication.user;
+    let supportedLaguanges = this.props.config.supported_languages;
     return (
       <div
         style={{
@@ -44,56 +88,90 @@ class Header extends Component {
         <div
           style={{
             marginRight: "10px",
-            width: "250px",
+            width: _.isEmpty(supportedLaguanges) ? "180px" : "250px",
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center"
           }}
         >
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <span
-              style={{
-                opacity: 0.3,
-                color: "#000000",
-                fontWeight: 500,
-                fontSize: "18px",
-                letterSpacing: "-0.03px",
-                lineHeight: "22px",
-                textålign: "right",
-                marginRight: "8px"
-              }}
+          {showSearchInputIcon ? (
+            <div className={"search-box "}>
+              <Input
+                style={{ display: "inline" }}
+                suffix={
+                  <Icon
+                    type="search"
+                    onClick={() => this.onSearch(searchInput)}
+                    className="text-anchor"
+                    style={{ color: "rgba(0,0,0,.25)" }}
+                  />
+                }
+                value={searchInput}
+                placeholder={this.props.intl.formatMessage({
+                  id: "commonTextInstances.search"
+                })}
+                onChange={this.onSearchChange}
+                ref={node => (this.searchInput = node)}
+                onKeyPress={this.handleKeyPress}
+              />
+              {/* <Icon onClick={this.toggleSearchIcon} type="close-circle" /> */}
+            </div>
+          ) : (
+            <div
+              style={{ display: "flex", alignItems: "end" }}
+              onClick={this.toggleSearchIcon}
             >
-              Search
-            </span>
-            <i
-              className="material-icons text-middle"
-              style={{ fontSize: 24, color: "#000000", opacity: 0.3 }}
-            >
-              search
-            </i>
-          </div>
-          <span
-            style={{
-              opacity: 0.3,
-              color: "#000000",
-              fontWeight: 500,
-              fontSize: "18px",
-              letterSpacing: "-0.03px",
-              lineHeight: "22px",
-              textålign: "right"
-            }}
-          >
-            EN
-          </span>
-          {user ? (
-            <Tooltip title={user.email} placement="leftBottom">
+              <span
+                style={{
+                  opacity: 0.3,
+                  color: "#000000",
+                  fontWeight: 500,
+                  fontSize: "18px",
+                  letterSpacing: "-0.03px",
+                  lineHeight: "22px",
+                  textålign: "right",
+                  marginRight: "8px"
+                }}
+              >
+                Search
+              </span>
               <i
                 className="material-icons text-middle"
-                style={{ fontSize: 36, color: "#000000", opacity: 0.3 }}
+                style={{ fontSize: 24, color: "#000000", opacity: 0.3 }}
               >
-                account_circle
+                search
               </i>
-            </Tooltip>
+            </div>
+          )}
+          {_.isEmpty(supportedLaguanges) || <SelectLanguage navbar={true} />}
+          {user ? (
+            <Dropdown
+              overlay={
+                <Menu>
+                  <Menu.Item
+                    key="logout"
+                    onClick={e => this.onLogout(e, "key")}
+                  >
+                    Logout
+                  </Menu.Item>
+                </Menu>
+              }
+              trigger={["click"]}
+            >
+              <Tooltip title={user.email} placement="leftBottom">
+                <i
+                  className="material-icons text-middle"
+                  style={{
+                    fontSize: 36,
+                    color: "#000000",
+                    opacity: 0.3,
+                    cursor: "pointer"
+                  }}
+                >
+                  account_circle
+                </i>
+              </Tooltip>
+            </Dropdown>
           ) : null}
         </div>
       </div>
---
+++ src/modules/header/components/SelectLanguage.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Select, Tooltip } from "antd";
+import { languageActions } from "../actions";
+import _ from "lodash";
+import languages from "../intlLanguages";
+import { languageConstants } from "../constants";
+
+const Option = Select.Option;
+
+class SelectLanguage extends React.Component {
+  handleLanguageChange = value => {
+    if (this.props.authentication.user) {
+      this.props.dispatch(languageActions.updateUserLanguage(value));
+    }
+  };
+  render() {
+    if (this.props.languageSelector.loading) {
+      console.log(`Language change in progress, will need reload`);
+      this.languageChangeInprogress = true;
+    } else if (this.languageChangeInprogress) {
+      console.log(`Language change done, will reload`);
+      window.location.reload();
+    }
+    let user = this.props.authentication.user;
+    let preferredLanguage =
+      this.props.languageSelector.language ||
+      (user && user.prefered_language) ||
+      (navigator.languages && navigator.languages[0]) ||
+      navigator.language ||
+      navigator.userLanguage ||
+      languageConstants.DEFAULT_LOCALE;
+    if (!languages.endonyms[preferredLanguage]) {
+      preferredLanguage = preferredLanguage.split("-")[0];
+    }
+    let supportedLaguanges = this.props.config.supported_languages;
+    if (!_.includes(supportedLaguanges, preferredLanguage)) {
+      preferredLanguage = supportedLaguanges[0];
+    }
+    return (
+      <span>
+        <Select
+          defaultValue={preferredLanguage}
+          className="nav-lang-select"
+          onChange={this.handleLanguageChange}
+        >
+          {_.map(Object.keys(languages.endonyms), function(locale, index) {
+            return (
+              _.includes(supportedLaguanges, locale) && (
+                <Option key={locale} value={locale}>
+                  <Tooltip
+                    title={languages.endonyms[locale]}
+                    placement="leftTop"
+                  >
+                    <span
+                      style={{
+                        textTransform: "uppercase",
+                        opacity: 0.3,
+                        color: "#000000",
+                        fontSize: "16px",
+                        letterSpacing: "-0.03px",
+                        textAlign: "right"
+                      }}
+                    >
+                      {locale}
+                    </span>
+                  </Tooltip>
+                </Option>
+              )
+            );
+          })}
+        </Select>
+      </span>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { config, authentication, languageSelector } = state;
+  return {
+    config,
+    authentication,
+    languageSelector
+  };
+}
+
+export default connect(mapStateToProps)(SelectLanguage);
