6272e7106103fa8f2fb549f0a147c8d3b3ff3427
junaid7lone <junaid7lone@gmail.com>
2019-05-09 13:05:16 +0530

    removed locl api

--- src/js/_helpers/api-url-local.js
+++ src/js/_helpers/api-url-local.js
@@ -3,23 +3,20 @@ const getSite = site => {
   let hostSplit = host.split(".");
   let domain =
     document.location.protocol +
-    "//" +
-    hostSplit[0] +
-    "." +
+    "//api." +
     hostSplit[1] +
     "." +
     hostSplit[2] +
-    ":8000/api/v1/";
+    "/api/v1/";
   return domain;
 };
 
 const subDomainUrl = site => {
   let host = document.location.hostname;
   let hostSplit = host.split(".");
-  let domain = document.location.protocol + "//" + host + "/api/v1/";
+  let domain = document.location.protocol + "//" + host + "/api/v1/"; //props
   return domain;
 };
 
 export const baseUrl = getSite();
-
 export const baseUrl2 = subDomainUrl();
