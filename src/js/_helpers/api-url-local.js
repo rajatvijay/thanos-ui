00341663f5f5460ff9a3c75237a7b8f9784c73fe
junaid7lone <junaid7lone@gmail.com>
2019-05-09 13:03:58 +0530

    lean workflow list

--- src/js/_helpers/api-url-local.js
+++ src/js/_helpers/api-url-local.js
@@ -3,20 +3,23 @@ const getSite = site => {
   let hostSplit = host.split(".");
   let domain =
     document.location.protocol +
-    "//api." +
+    "//" +
+    hostSplit[0] +
+    "." +
     hostSplit[1] +
     "." +
     hostSplit[2] +
-    "/api/v1/";
+    ":8000/api/v1/";
   return domain;
 };
 
 const subDomainUrl = site => {
   let host = document.location.hostname;
   let hostSplit = host.split(".");
-  let domain = document.location.protocol + "//" + host + "/api/v1/"; //props
+  let domain = document.location.protocol + "//" + host + "/api/v1/";
   return domain;
 };
 
 export const baseUrl = getSite();
+
 export const baseUrl2 = subDomainUrl();
