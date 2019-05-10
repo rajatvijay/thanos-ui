22ecf1c3b81b4ed018eaabae718ca6a8f4a7bdbe
Rajat Vijay <rajatvijay5@gmail.com>
2019-05-10 23:00:44 +0530

    refactored sidebar circle

--- src/js/modules/components/common/sidebar/SidebarCircle.js
+++ src/js/modules/components/common/sidebar/SidebarCircle.js
@@ -1,28 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SidebarCircle extends Component {
-  render() {
-    const { value, innerColour } = this.props;
-
-    return (
-      <span
-        style={{
-          borderRadius: "50%",
-          backgroundColor: innerColour,
-          color: "white",
-          margin: "0px 5px",
-          fontSize: 10,
-          width: 25,
-          height: 25,
-          lineHeight: "25px",
-          textAlign: "center",
-          display: "inline-block"
-        }}
-      >
-        {value}
-      </span>
-    );
-  }
-}
+const SidebarCircle = ({ value, innerColour }) => {
+  return (
+    <span
+      style={{
+        borderRadius: "50%",
+        backgroundColor: innerColour,
+        color: "white",
+        margin: "0px 5px",
+        fontSize: 10,
+        width: 25,
+        height: 25,
+        lineHeight: "25px",
+        textAlign: "center",
+        display: "inline-block"
+      }}
+    >
+      {value}
+    </span>
+  );
+};
 
 export default SidebarCircle;
