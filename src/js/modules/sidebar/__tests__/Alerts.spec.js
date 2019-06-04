493ca8faa5475fba35750859f780b515a63309a7
Rajat Vijay <rajatvijay5@gmail.com>
2019-06-04 15:21:11 +0530

    refactored alerts using tests

--- src/js/modules/sidebar/__tests__/Alerts.spec.js
+++ src/js/modules/sidebar/__tests__/Alerts.spec.js
@@ -37,13 +37,13 @@ test("should render all the alert sub categories", () => {
     name: "Fake Alert",
     count: 0,
     sub_categories: [
-      { name: "Fake Alert Sub Category 1", count: 572 },
-      { name: "Fake Alert Sub Category 2", count: 573 },
-      { name: "Fake Alert Sub Category 3", count: 574 },
-      { name: "Fake Alert Sub Category 4", count: 575 },
-      { name: "Fake Alert Sub Category 5", count: 576 },
-      { name: "Fake Alert Sub Category 6", count: 577 },
-      { name: "Fake Alert Sub Category 7", count: 578 }
+      { id: 524, name: "Fake Alert Sub Category 1", count: 572 },
+      { id: 525, name: "Fake Alert Sub Category 2", count: 573 },
+      { id: 526, name: "Fake Alert Sub Category 3", count: 574 },
+      { id: 527, name: "Fake Alert Sub Category 4", count: 575 },
+      { id: 528, name: "Fake Alert Sub Category 5", count: 576 },
+      { id: 529, name: "Fake Alert Sub Category 6", count: 577 },
+      { id: 523, name: "Fake Alert Sub Category 7", count: 578 }
     ]
   };
   const { getAllByTestId } = render(<Alerts item={fakeAlert} />);
@@ -56,7 +56,7 @@ test("should render the name of the  sub category", () => {
   const fakeAlert = {
     name: "Fake Alert",
     count: 576,
-    sub_categories: [{ name: "Fake Alert Sub Category 1", count: 572 }]
+    sub_categories: [{ id: 524, name: "Fake Alert Sub Category 1", count: 572 }]
   };
   const { getByText } = render(<Alerts item={fakeAlert} />);
   expect(getByText(/fake alert sub category/i)).toBeInTheDocument();
@@ -66,7 +66,7 @@ test("should render the workflow count for the  sub category", () => {
   const fakeAlert = {
     name: "Fake Alert",
     count: 576,
-    sub_categories: [{ name: "Fake Alert Sub Category 1", count: 572 }]
+    sub_categories: [{ id: 524, name: "Fake Alert Sub Category 1", count: 572 }]
   };
   const { getByText } = render(<Alerts item={fakeAlert} />);
   expect(getByText(String(572))).toBeInTheDocument();
@@ -76,7 +76,7 @@ test("should call onSelect with alert sub cateogry when a it is clicked", () => 
   const fakeAlert = {
     name: "Fake Alert",
     count: 576,
-    sub_categories: [{ name: "Fake Alert Sub Category 1", count: 572 }]
+    sub_categories: [{ id: 524, name: "Fake Alert Sub Category 1", count: 572 }]
   };
   const onSelect = jest.fn();
   const { getByText } = render(<Alerts item={fakeAlert} onSelect={onSelect} />);
@@ -84,6 +84,7 @@ test("should call onSelect with alert sub cateogry when a it is clicked", () => 
   element.click();
   expect(onSelect).toHaveBeenCalledTimes(1);
   expect(onSelect).toHaveBeenCalledWith({
+    id: 524,
     name: "Fake Alert Sub Category 1",
     count: 572
   });
--- src/js/modules/sidebar/components/Alerts.js
+++ src/js/modules/sidebar/components/Alerts.js
@@ -2,86 +2,106 @@ import React, { Component } from "react";
 import Collapsible from "react-collapsible";
 import { Icon } from "antd";
 import CountWidget from "./CountWidget";
+import styled from "@emotion/styled";
+import { css } from "emotion";
 
 class Alerts extends Component {
-  state = { collapse: false };
+  state = { collapsed: false };
 
-  renderList = () => {
+  renderSubCategoryList = () => {
     const { onSelect, item, selected } = this.props;
 
     if (item.sub_categories) {
-      return item.sub_categories.map(item => (
-        <li
-          onClick={e => {
-            e.stopPropagation();
-            onSelect(item);
-          }}
+      return item.sub_categories.map(subCategory => (
+        <StyledAlertSubCategoryListItem
+          key={subCategory.id}
+          onClick={e => onSelect(subCategory)}
           data-testid="alerts-list-item"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            margin: "4px 18px 4px 25px",
-            backgroundColor:
-              item["name"] === selected
-                ? "#1489D2"
-                : "rgba(255, 255, 255, 0.15)",
-            borderRadius: "30px",
-            padding: "3px 3px 0px 12px",
-            color: "white",
-            cursor: "pointer"
-          }}
+          isSelected={subCategory.name === selected}
         >
-          <span>{item.name}</span>
-          <CountWidget innerColour="#D40000" value={item.count} />
-        </li>
+          <span>{subCategory.name}</span>
+          <CountWidget innerColour="#D40000" value={subCategory.count} />
+        </StyledAlertSubCategoryListItem>
       ));
     }
+    return null;
+  };
+
+  toggleCollapsible = () => {
+    this.setState(({ collapsed }) => ({ collapsed: !collapsed }));
   };
 
   render() {
-    const { collapse } = this.state;
+    const { collapsed } = this.state;
     const { item } = this.props;
 
     return (
-      <li
-        style={{
-          opacity: collapse && 1,
-          paddingBottom: collapse ? 30 : 0,
-          backgroundColor: collapse ? "#093050" : "#104775",
-          transitionDuration: "150ms"
-        }}
-        onClick={() => this.setState({ collapse: !collapse })}
+      <StyledAlertListItem
+        collapsed={collapsed}
+        onClick={this.toggleCollapsible}
       >
         <div
-          style={{
-            borderTop: "1px solid rgba(0, 0, 0, 0.3)",
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "10px 20px",
-            cursor: "pointer"
-          }}
+          className={css`
+            display: flex;
+            justify-content: space-between;
+          `}
         >
           <div>
             <Icon
-              style={{ margin: "0px 5px 0px -5px", color: "white" }}
+              className={css`
+                margin: 0px 5px 0px -5px;
+                color: white;
+              `}
               type="caret-right"
-              rotate={collapse ? 90 : 0}
+              rotate={collapsed ? 90 : 0}
             />
-            <span style={{ fontSize: 16, color: "#CFDAE3" }}>{item.name}</span>
+            <span
+              className={css`
+                font-size: 16px;
+                color: #cfdae3;
+              `}
+            >
+              {item.name}
+            </span>
           </div>
-          <div>
-            {item.count > 0 && (
-              <CountWidget innerColour="#D40000" value={item.count} />
-            )}
-          </div>
+          {item.count > 0 && (
+            <CountWidget innerColour="#D40000" value={item.count} />
+          )}
         </div>
 
-        <Collapsible open={collapse}>
-          <ul style={{ padding: 0 }}>{this.renderList()}</ul>
+        <Collapsible open={collapsed}>
+          <ul
+            className={css`
+              padding: 0;
+            `}
+          >
+            {this.renderSubCategoryList()}
+          </ul>
         </Collapsible>
-      </li>
+      </StyledAlertListItem>
     );
   }
 }
 
 export default Alerts;
+
+const StyledAlertListItem = styled.li`
+  padding-bottom: ${({ collapsed }) => (collapsed ? 30 : 0)};
+  background-color: ${({ collapsed }) => (collapsed ? "#093050" : "#104775")};
+  transition-duration: 150ms;
+  border-top: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+
+const StyledAlertSubCategoryListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 6px;
+  background-color: ${({ isSelected }) =>
+    isSelected ? "#1489D2" : "rgba(255, 255, 255, 0.15)"};
+  border-radius: 30px;
+  padding: 3px 3px 0px 12px;
+  color: white;
+  cursor: pointer;
+`;
