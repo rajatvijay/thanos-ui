b7309708a93eff550a89a4d3df8aaf17d2f18941
Rajat Vijay <rajatvijay5@gmail.com>
2019-07-01 15:45:40 +0530

    alerts visibility + status values visibility in filters popup

--- src/js/modules/filter/components/FilterPopup.js
+++ src/js/modules/filter/components/FilterPopup.js
@@ -52,13 +52,15 @@ class FilterPopup extends Component {
   };
 
   getStatusTypes = () => {
-    try {
-      const allStatuses = this.props.workflowFilterType.statusType;
-      const kindId = this.props.workflowKindValue.selectedKindValue.id;
-      return allStatuses.filter(status => status.workflow_kind === kindId);
-    } catch (e) {
-      return [];
-    }
+    // TODO: Commenting out the filtering of status based on kind ID
+    // try {
+    //   const allStatuses = this.props.workflowFilterType.statusType;
+    //   const kindId = this.props.workflowKindValue.selectedKindValue.id;
+    //   return allStatuses.filter(status => status.workflow_kind === kindId);
+    // } catch (e) {
+    //   return [];
+    // }
+    return this.props.workflowFilterType.statusType;
   };
 
   render() {
