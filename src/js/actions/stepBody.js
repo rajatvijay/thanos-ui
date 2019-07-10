4172733eed5b718ac3ab3d2046748b6c3f448a1a
pankaj <gargpankaj901@gmail.com>
2019-07-10 22:16:03 +0530

    mock to api

--- src/js/actions/stepBody.js
+++ src/js/actions/stepBody.js
@@ -20,27 +20,38 @@ const {
 const postStepUser = obj => async dispatch => {
   dispatch({ type: POST_STEP_USER_LOADING, stepId: obj.stepId });
 
-  //   const res = await axios.post(`${baseUrl}api/v1/step-user-tags`, obj, {
-  //     credentials: "include",
-  //     header: {
-  //       headers: authHeader.get()
+  // const res = await axios.post(`${baseUrl}step-user-tags`, obj, {
+  //   credentials: "include",
+  //   header: {
+  //     headers: authHeader.get()
+  //   }
+  // });
+
+  const requestOptions = {
+    method: "POST",
+    headers: authHeader.post(),
+    credentials: "include",
+    body: JSON.stringify(obj)
+  };
+
+  const url = `${baseUrl}step-user-tags/`;
+  let res = await fetch(url, requestOptions);
+  res = await res.json();
+
+  // const res = await new Promise(function(resolve, reject) {
+  //   resolve({
+  //     payload: {
+  //       id: 1, // this resource id
+  //       tag: "responsible",
+  //       step: 1,
+  //       user: {
+  //         name: "Rajat",
+  //         email: "rajat@gmail.com"
+  //       }
   //     }
   //   });
-
-  const res = await new Promise(function(resolve, reject) {
-    resolve({
-      payload: {
-        id: 1, // this resource id
-        tag: "responsible",
-        step: 1,
-        user: {
-          name: "Rajat",
-          email: "rajat@gmail.com"
-        }
-      }
-    });
-  });
-  console.log("resget", res);
+  // });
+  // console.log("resget", res);
 
   try {
     dispatch({
@@ -58,34 +69,46 @@ const postStepUser = obj => async dispatch => {
 };
 
 export const getStepUsers = stepId => async dispatch => {
-  // const res = await axios.get(`${baseUrl}api/v1/steps/${stepId}/get-users-with-edit-access/`,{
+  dispatch({ type: GET_STEP_USERS_LOADING, stepId });
+
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader.get(),
+    credentials: "include"
+    //body: JSON.stringify({ to_language: payload })
+  };
+
+  const url = `${baseUrl}steps/${stepId}/get-users-with-edit-access/`;
+  let res = await fetch(url, requestOptions);
+  res = await res.json();
+
+  // const res = await axios.get(`${baseUrl}steps/322633/get-users-with-edit-access/`,{
   //         credentials: "include",
   //         headers: {
   //           headers: authHeader.get()
   //         }
   // });
-  dispatch({ type: GET_STEP_USERS_LOADING, stepId });
 
-  const res = await new Promise(function(resolve, reject) {
-    resolve({
-      payload: [
-        {
-          id: 1,
-          email: "sai@thevetted.com",
-          name: "Sai"
-        },
-        {
-          id: 5,
-          email: "brenkerts@dnb.com",
-          name: "ram"
-        }
-      ]
-    });
-  });
+  // const res = await new Promise(function(resolve, reject) {
+  //   resolve({
+  //     payload: [
+  //       {
+  //         id: 1,
+  //         email: "sai@thevetted.com",
+  //         name: "Sai"
+  //       },
+  //       {
+  //         id: 5,
+  //         email: "brenkerts@dnb.com",
+  //         name: "ram"
+  //       }
+  //     ]
+  //   });
+  // });
   console.log("resget", res);
 
   try {
-    dispatch({ type: GET_STEP_USERS_SUCCESS, payload: res.payload, stepId });
+    dispatch({ type: GET_STEP_USERS_SUCCESS, payload: res, stepId });
   } catch (err) {
     dispatch({ type: GET_STEP_USERS_FAILURE, payload: err, stepId });
   }
@@ -94,6 +117,17 @@ export const getStepUsers = stepId => async dispatch => {
 const deleteStepUser = (stepId, id) => async dispatch => {
   dispatch({ type: DELETE_STEP_USER_LOADING, stepId: stepId });
 
+  const requestOptions = {
+    method: "DELETE",
+    headers: authHeader.post(),
+    credentials: "include"
+    // body: JSON.stringify(obj)
+  };
+
+  const url = `${baseUrl}/api/v1/step-user-tags/${id}`;
+  let res = await fetch(url, requestOptions);
+  res = await res.json();
+
   //   const res = await axios.delete(`${baseUrl}/api/v1/step-user-tags/${id}`, {
   //     credentials: "include",
   //     header: {
@@ -101,9 +135,9 @@ const deleteStepUser = (stepId, id) => async dispatch => {
   //     }
   //   });
 
-  const res = await new Promise(function(resolve, reject) {
-    resolve("success");
-  });
+  // const res = await new Promise(function(resolve, reject) {
+  //   resolve("success");
+  // });
   // console.log("resget", res);
 
   try {
@@ -116,33 +150,45 @@ const deleteStepUser = (stepId, id) => async dispatch => {
 const getAssignedUser = stepId => async dispatch => {
   dispatch({ type: POST_STEP_USER_LOADING, stepId: stepId });
 
-  //   const res = await axios.delete(`${baseUrl}/api/v1/step-user-tags/?step=${stepId}`, {
-  //     credentials: "include",
-  //     header: {
-  //       headers: authHeader.get()
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader.get(),
+    credentials: "include"
+    //body: JSON.stringify({ to_language: payload })
+  };
+
+  const url = `${baseUrl}step-user-tags/?step=${stepId}`;
+  let res = await fetch(url, requestOptions);
+  res = await res.json();
+
+  // const res = await axios.get(`${baseUrl}step-user-tags/?step=${stepId}`, {
+  //   credentials: "include",
+  //   header: {
+  //     headers: authHeader.get(),"Content-Type":"application/json"
+  //   }
+  // });
+
+  // const res = await new Promise(function(resolve, reject) {
+  //   resolve({
+  //     payload: {
+  //       count: 1,
+  //       next: null,
+  //       previous: null,
+  //       results: []
   //     }
   //   });
-
-  const res = await new Promise(function(resolve, reject) {
-    resolve({
-      payload: {
-        count: 1,
-        next: null,
-        previous: null,
-        results: []
-      }
-    });
-  });
-  // console.log("resget", res);
+  // });
+  console.log("resget", res);
 
   try {
-    if (res.payload.results.length) {
+    if (res.results.length) {
       dispatch({
         type: POST_STEP_USER_SUCCESS,
-        payload: res.payload.results[0],
+        payload: res.results[0],
         stepId: stepId
       });
     } else {
+      console.log("getting");
       dispatch(getStepUsers(stepId));
     }
   } catch (err) {
