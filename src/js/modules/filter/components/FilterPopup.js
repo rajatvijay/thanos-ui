7cd1207ab6b1b3e26fe458a5693b8e04d1798813
junaid7lone <junaid7lone@gmail.com>
2019-07-16 08:37:27 +0530

    Added Advanced filter multiple conditions and clear option.

--- src/js/modules/filter/components/FilterPopup.js
+++ src/js/modules/filter/components/FilterPopup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Modal, Button, Input, Cascader, Divider } from "antd";
+import { Modal, Button, Input, Cascader, Divider, Icon } from "antd";
 import DropdownFilter from "./DropdownFilter";
 import { connect } from "react-redux";
 import { css } from "emotion";
@@ -20,7 +20,8 @@ class FilterPopup extends Component {
     operator: undefined,
     text: "",
     field: undefined,
-    showError: false
+    showError: false,
+    advFitlers: []
   };
 
   onClear = () => {
@@ -33,24 +34,72 @@ class FilterPopup extends Component {
     });
   };
 
+  //CLEAR ALL ITEMS FROM ADVANCED FILTERS
+  onAdvClear = () => {
+    this.setState({ advFitlers: [] });
+    this.applyAdvFilters();
+    this.props.onModalClose();
+  };
+
   updateAdvanceFilterTextValue = e => {
     const { value } = e.target;
     this.setState({ text: value });
   };
 
+  //ON ADVANCED FILTER APPLY EVENT
   onApply = () => {
     const { field, text, operator } = this.state;
     const { applyFilters, onModalClose } = this.props;
+    let advFitlers = this.state.advFitlers;
 
     if (field && text && operator) {
       const fieldValue = field[field.length - 1];
+      const currentFitler = {
+        field: fieldValue,
+        operator: operator,
+        text: text
+      };
 
-      applyFilters("answer", `${fieldValue}__${operator}__${text}`);
+      advFitlers.push(currentFitler);
+      this.setState({ advFitlers: advFitlers });
+      this.applyAdvFilters();
+      this.clearAdvancedFilterFileds();
       this.setState({ showError: false });
       onModalClose();
     } else {
       this.setState({ showError: true });
     }
+  };
+
+  //CLEAR ADVANCED FILTER FILTEDS
+  clearAdvancedFilterFileds = () => {
+    this.setState({ field: null, operator: null, text: null });
+  };
+
+  //CONSTRUCST FILTER STIRNG FROM ADVANCED FILTER STATE AND DISPATCH
+  applyAdvFilters = () => {
+    let advFitlersString = "";
+
+    if (this.state.advFitlers.length > 0) {
+      this.state.advFitlers.forEach((filter, index) => {
+        const { field, text, operator } = filter;
+        if (index > 0) {
+          advFitlersString = advFitlersString.concat("|");
+        }
+        const currentFitler = `${field}__${operator}__${text}`;
+        advFitlersString = advFitlersString.concat(currentFitler);
+      });
+    }
+
+    this.props.applyFilters("answer", advFitlersString);
+  };
+
+  //REMOVE INDIVISUAL FILTER ITEM
+  removeAdvFilter = key => {
+    let { advFitlers } = this.state;
+    advFitlers.splice(key, 1);
+    this.setState({ advFitlers: advFitlers });
+    this.applyAdvFilters();
   };
 
   getStatusTypes = () => {
@@ -164,11 +213,53 @@ class FilterPopup extends Component {
           </div>
 
           <div style={{ marginTop: "46px" }}>
-            <span style={{ marginLeft: 28, color: "#138BD6" }}>
-              ADVANCED FILTERS
-            </span>
+            <div
+              style={{
+                justifyContent: "space-between",
+                display: "flex",
+                marginLeft: "28px"
+              }}
+            >
+              <span style={{ color: "#138BD6", cursor: "pointer" }}>
+                ADVANCED FILTERS
+              </span>
+              <span
+                style={{ cursor: "pointer", marginRight: "38px" }}
+                onClick={this.onAdvClear}
+              >
+                CLEAR
+              </span>
+            </div>
 
             <Divider />
+            {this.state.advFitlers.length > 0 ? (
+              <div className="advanced-fitlers-list" style={{ margin: 28 }}>
+                {this.state.advFitlers.map((item, index) => {
+                  return (
+                    <span
+                      key={index}
+                      className="t-12 text-middle text-light  ant-tag v-tag pd-right"
+                      style={{
+                        wordBreak: "break-word",
+                        height: "auto",
+                        whiteSpace: "normal"
+                      }}
+                    >
+                      <span
+                        title="Remove this filter"
+                        className="pd-ard float-right"
+                        onClick={e => this.removeAdvFilter(index)}
+                      >
+                        <Icon type="close" />
+                      </span>
+                      <span className="pd-right-sm">{item.field}</span>
+                      <span className="pd-right-sm">{item.operator}</span>
+                      <span className="pd-right-sm">{item.text}</span>
+                    </span>
+                  );
+                })}
+              </div>
+            ) : null}
 
             <div style={{ margin: 28 }}>
               <Cascader
