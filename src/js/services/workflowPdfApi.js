93204247a8ab026130b978464964047b198f21ab
Rajat Vijay <rajatvijay5@gmail.com>
2019-08-12 17:19:58 +0530

    int. commit

--- src/js/services/workflowPdfApi.js
+++ src/js/services/workflowPdfApi.js
@@ -1,6 +1,6 @@
-export const fetchWorkflowDetails = () => {
+export const fetchWorkflowDetails = stepTag => {
   return fetch(
-    "https://af3e2827-dd97-40a5-bb17-befe64b64f54.mock.pstmn.io/api/v1/workflow/pdf/config/2133/?trigger_step=step_tag"
+    `https://af3e2827-dd97-40a5-bb17-befe64b64f54.mock.pstmn.io/api/v1/workflow/pdf/config/2133/?trigger_step=${stepTag}`
   ).then(response => response.json());
 };
 
