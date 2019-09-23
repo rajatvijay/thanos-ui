75a9a2fe2b75e94513cd786ecc086b2928969623
Rajat Vijay <rajatvijay5@gmail.com>
2019-09-23 13:23:28 +0530

    more tests for workflow sorter

--- src/modules/workflowList/filters/__tests__/WorkflowSorter.js
+++ src/modules/workflowList/filters/__tests__/WorkflowSorter.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { renderWithRedux } from "../../../common/utils/testUtils";
 import WorkflowSorter from "../components/WorkflowSorter";
+import { FetchMock, fetchMock } from "@react-mock/fetch";
+import { fireEvent } from "@testing-library/react";
+import {
+  WORKLFOW_ASC_SORT_PARAM,
+  WORKLFOW_DESC_SORT_PARAM
+} from "../../constants";
 
 test("should not render anything if kind is not selected", () => {
   const { queryByText } = renderWithRedux(<WorkflowSorter />, {
@@ -24,4 +30,124 @@ test("should not render anything if kind not selected but sorting is disabled", 
     }
   });
   expect(queryByText(/sort/i)).toBeNull();
+});
+
+test("should render 'sort:risk' text when sorting is enabled", () => {
+  const { queryByText } = renderWithRedux(<WorkflowSorter />, {
+    initialState: {
+      workflowList: {
+        selectedWorkflowFilters: {
+          kind: {
+            is_sorting_field_enabled: true
+          }
+        }
+      }
+    }
+  });
+  expect(queryByText(/sort/i)).toBeInTheDocument();
+  expect(queryByText(/risk/i)).toBeInTheDocument();
+});
+
+test("should call the api with asc order when sorting is clicked once and render proper sorting icon", () => {
+  const API_URL = "path:/api/v1/workflows-list/";
+  const { queryByText, queryByTestId } = renderWithRedux(
+    <FetchMock mocks={[{ matcher: API_URL, method: "GET", response: 200 }]}>
+      <WorkflowSorter />
+    </FetchMock>,
+    {
+      initialState: {
+        workflowList: {
+          selectedWorkflowFilters: {
+            kind: {
+              is_sorting_field_enabled: true
+            }
+          }
+        }
+      }
+    }
+  );
+  const sortElement = queryByText(/sort/i);
+  fireEvent.click(sortElement);
+
+  const [url] = fetchMock.lastCall(API_URL, "GET");
+  const orderingParam = new URL(url).searchParams.get("ordering");
+  expect(orderingParam).toBe(WORKLFOW_ASC_SORT_PARAM);
+  expect(queryByTestId(/sorting-up/)).toBeInTheDocument();
+});
+
+test("should call the api with desc order when sorting is clicked twice and render proper sorting icon", () => {
+  const API_URL = "path:/api/v1/workflows-list/";
+  const { queryByText, queryByTestId } = renderWithRedux(
+    <FetchMock mocks={[{ matcher: API_URL, method: "GET", response: 200 }]}>
+      <WorkflowSorter />
+    </FetchMock>,
+    {
+      initialState: {
+        workflowList: {
+          selectedWorkflowFilters: {
+            kind: {
+              is_sorting_field_enabled: true
+            }
+          }
+        }
+      }
+    }
+  );
+
+  // Click twice
+  const sortElement = queryByText(/sort/i);
+  fireEvent.click(sortElement);
+  fireEvent.click(sortElement);
+
+  const [url] = fetchMock.lastCall(API_URL, "GET");
+  const orderingParam = new URL(url).searchParams.get("ordering");
+  expect(orderingParam).toBe(WORKLFOW_DESC_SORT_PARAM);
+  expect(queryByTestId(/sorting-down/)).toBeInTheDocument();
+});
+
+test("should not call the api with sorting param when sorting is clicked thrice", () => {
+  const API_URL = "path:/api/v1/workflows-list/";
+  const { queryByText } = renderWithRedux(
+    <FetchMock mocks={[{ matcher: API_URL, method: "GET", response: 200 }]}>
+      <WorkflowSorter />
+    </FetchMock>,
+    {
+      initialState: {
+        workflowList: {
+          selectedWorkflowFilters: {
+            kind: {
+              is_sorting_field_enabled: true
+            }
+          }
+        }
+      }
+    }
+  );
+
+  // Click twice
+  const sortElement = queryByText(/sort/i);
+  fireEvent.click(sortElement);
+  fireEvent.click(sortElement);
+  fireEvent.click(sortElement);
+
+  const [url] = fetchMock.lastCall(API_URL, "GET");
+  const orderingParam = new URL(url).searchParams.get("ordering");
+  expect(orderingParam).toBe(null);
+});
+
+test("should not render any sorting icon initially", () => {
+  const { queryByTestId } = renderWithRedux(<WorkflowSorter />, {
+    initialState: {
+      workflowList: {
+        selectedWorkflowFilters: {
+          kind: {
+            is_sorting_field_enabled: true
+          }
+        }
+      }
+    }
+  });
+
+  expect(queryByTestId(/sorting-up/)).toBeNull();
+  expect(queryByTestId(/sorting-down/)).toBeNull();
 });
