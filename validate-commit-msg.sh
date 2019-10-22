4905adddc0441b9be6dc2531b84f8d74deae443d
Abhinav Dabral <abhinavdabral@users.noreply.github.com>
2019-10-22 11:06:02 +0530

    VET-6812 Husky commit-msg to support other prefixes (#1191)

--- validate-commit-msg.sh
+++ validate-commit-msg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MESSAGE=$(cat $HUSKY_GIT_PARAMS)
-PATTERN="^\[?VET\-([0-9]+)\]? (.*)$"
+PATTERN="^(VET-[0-9]+|wip|Merge) (.*)$"
 
 if ! [[ $MESSAGE =~ $PATTERN ]]; then
-    echo "ERROR: Commit message did not match 'VET-123 subject' or '[VET-123] subject'"
+    echo "ERROR: Commit message does not have any of the following prefixes: VET-1234, wip, Merge"
     exit 1
 fi
